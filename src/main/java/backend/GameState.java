c0ad88277b9525083c7c3dd03f4db7d0607ce579
P0lyFish <tranthephong32@gmail.com>
2020-11-05 00:09:51 +0700

    update GameState

--- src/main/java/backend/GameState.java
+++ src/main/java/backend/GameState.java
@@ -1,29 +1,47 @@
 package main.java.backend;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class GameState {
-    private final float UPDATE_PERIOD = 0;
+    public final float BOMB_EXPLOSION_TIME = 0;
+    public final float DEFAULT_BLAST_RANGE = 0;
 
     List<Entity> entities;
+    Stack<KeyEvent> playerInputStack;
 
     public GameState() {}
 
-    public GameState(String mapPath) {
-
-    }
+    public GameState(String mapPath) {}
 
     public void addEntity(Entity e) {
         entities.add(e);
     }
 
-    void takePlayerInput() {
+    public List<Entity> getEntityList() {
+        return this.entities;
+    }
 
+    public void addPlayerInput(KeyEvent key) {
+        while (!playerInputStack.empty()) {
+            playerInputStack.pop();
+        }
+        playerInputStack.add(key);
+    }
+
+    public boolean inputStackIsEmpty() {
+        return playerInputStack.empty();
+    }
+
+    public KeyEvent popPlayerInputStack() {
+        return playerInputStack.pop();
     }
 
     public void refresh() {
-        takePlayerInput();
-
         for (Entity e : entities) {
             e.updateGameState(this);
         }
