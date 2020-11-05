23321b584db60df8a2f7d155804ce0f53eac3ba6
P0lyFish <tranthephong32@gmail.com>
2020-11-05 21:28:16 +0700

    update Entity

--- src/main/java/backend/Entity.java
+++ src/main/java/backend/Entity.java
@@ -6,11 +6,48 @@ import main.java.utils.GridPosition;
 
 
 public abstract class Entity {
+    protected final float REMAINING_TIME_MAX = 2000;
+    protected final float REMAINING_TIME_MID = 1300;
+    protected final float REMAINING_TIME_MIN = 600;
+
+    protected enum Status {
+        normal, vanishing, vanished;
+    }
+
     protected GridPosition position;
     protected boolean visible;
     protected boolean blocked;
+    protected boolean destroyable;
+    protected Status status = Status.normal;
+    protected float timeUntilVanish;
 
     public Entity() {}
+
+    public Entity(GridPosition position, boolean visible, boolean blocked,
+                  boolean destroyable, float timeUntilVanish) {
+        this.position = position;
+        this.visible = visible;
+        this.blocked = blocked;
+        this.timeUntilVanish = timeUntilVanish;
+    }
+
+    protected void decreaseTimeUntilVanish(float delta) {
+        if (status == Status.vanishing) {
+            timeUntilVanish -= delta;
+        }
+
+        if (timeUntilVanish < 0) {
+            status = Status.vanished;
+        }
+    }
+
+    protected Status getStatus() {
+        return status;
+    }
+
+    public void destroy() {
+        status = Status.vanishing;
+    }
 
     public GridPosition getPosition() {
         return this.position;
