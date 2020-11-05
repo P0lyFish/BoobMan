f052b9e965c5f54e59b120e4412a640593530af7
P0lyFish <tranthephong32@gmail.com>
2020-11-05 12:23:35 +0700

    update GameState

--- src/main/java/backend/GameState.java
+++ src/main/java/backend/GameState.java
@@ -1,22 +1,38 @@
 package main.java.backend;
 
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import main.java.backend.agents.PlayerAgent;
+import main.java.backend.static_entities.Portal;
+import main.java.utils.GameStatus;
 
+import java.io.*;
 import java.util.List;
-import java.util.Queue;
 import java.util.Stack;
 
 public class GameState {
     public final float BOMB_EXPLOSION_TIME = 0;
     public final float DEFAULT_BLAST_RANGE = 0;
+    private GameStatus status = GameStatus.PLAYING;
+    private int mapID;
+    private int numRows;
+    private int numCols;
 
     List<Entity> entities;
     Stack<KeyEvent> playerInputStack;
 
-    public GameState() {}
+    public GameState() {
+    }
 
-    public GameState(String mapPath) {}
+    public GameState(String mapPath) throws IOException {
+        File mapFile = new File(mapPath);
+        try {
+            FileReader fin = new FileReader(mapFile);
+            BufferedReader reader = new BufferedReader(fin);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
 
     public void addEntity(Entity e) {
         entities.add(e);
@@ -42,8 +58,23 @@ public class GameState {
     }
 
     public void refresh() {
+        if (status != GameStatus.PLAYING) {
+            return;
+        }
+
         for (Entity e : entities) {
             e.updateGameState(this);
+        }
+
+        for (Entity e : entities) {
+            if (e instanceof Portal && ((Portal) e).isPassed()) {
+                status = GameStatus.WIN;
+                break;
+            }
+            if (e instanceof PlayerAgent && ((PlayerAgent) e).isDeath()) {
+                status = GameStatus.LOSE;
+                break;
+            }
         }
     }
 }
